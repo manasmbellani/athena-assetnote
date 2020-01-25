30bfbe2949f8d8d8f87d6948007d7dfddd6b5c9f
Manas Bellani <edwards.aus@hotmail.com>
2020-01-26 08:30:09 +1100

    added clearcut input and output files

--- list_assets_from_assetnote_json_extract.sh
+++ list_assets_from_assetnote_json_extract.sh
@@ -3,11 +3,15 @@
 # This script will extract all the assets from the out-assetnote-assets.json file
 # 
 if [ $# -lt 1 ]; then
-    echo "[*] $0 run [assets-json-file=out-assetnote-assets.json]"
+    echo "[*] $0 run [assets-json-file=out-assetnote-assets.json] [outfile=out-assetnote-assets.csv]"
     exit
 fi
-assets_json_file="out-assetnote-assets.json"
+assets_json_file=${1:-"out-assetnote-assets.json"}
+outfile=${2:-"out-assetnote-assets.csv"}
 
+echo "[*] Parsing input assets from: $assets_json_file and writing assets to outfile: $outfile..."
 jq -r ".[].node.host" "$assets_json_file" | \
     sort | \
-    uniq
+    uniq | \
+    tee "$outfile"
+
