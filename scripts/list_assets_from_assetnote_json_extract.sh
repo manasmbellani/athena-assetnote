eeebaa9d76f84ab5eb7c38f68f004b672d55d255
Manas Bellani <edwards.aus@hotmail.com>
2020-02-24 12:32:46 +1100

    fixed a bug in script where arguments were not being read correctly

--- scripts/list_assets_from_assetnote_json_extract.sh
+++ scripts/list_assets_from_assetnote_json_extract.sh
@@ -6,8 +6,8 @@ if [ $# -lt 1 ]; then
     echo "[*] $0 run [assets-json-file=out-assetnote-assets.json] [outfile=out-assetnote-assets.csv]"
     exit
 fi
-assets_json_file=${1:-"out-assetnote-assets.json"}
-outfile=${2:-"out-assetnote-assets.csv"}
+assets_json_file=${2:-"out-assetnote-assets.json"}
+outfile=${3:-"out-assetnote-assets.csv"}
 
 echo "[*] Parsing input assets from: $assets_json_file and writing assets to outfile: $outfile..."
 jq -r ".[].node.host" "$assets_json_file" | \
