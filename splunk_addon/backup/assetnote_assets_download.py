329e68adf3bc25e570b4f6e7c9907094cc514a9c
Manas Bellani <edwards.aus@hotmail.com>
2020-04-03 23:35:03 +1100

    added a script to download updated set of assets with back off timer in place to deal with failures

--- splunk_addon/backup/assetnote_assets_download.py
+++ splunk_addon/backup/assetnote_assets_download.py
@@ -6,77 +6,189 @@ import sys
 import time
 import datetime
 
-"""Sleep time between individual page loads (per page to load)"""
-SLEEP_TIMEOUT_PER_PAGE = 2
-
 """Special prefix added to each log message"""
 LOG_PREFIX = "AssetNote"
 
-"""Number of exposures to load per page"""
-EXPOSURES_PER_PAGE_COUNT = 20
+"""Number of assets to load per page"""
+ASSETS_PER_PAGE_COUNT = 25
 
-"""Limit the number of pages returned with exposures (for testing)"""
-LIMIT_PAGES_RETURNED = 0
+"""Limit the number of pages returned with assets (for testing.
+If set to 0, then all the pages are returned."""
+LIMIT_PAGES_RETURNED = 2
 
-# Graph Query templates to pull vulnerability and indicators
-EXPOSURES_GRAPHQL_QUERY_TEMPLATE = """
+# Graphql Query template to pull down assets. 
+# {page_count} is set and {page_num} is incremented to pull down the assets 
+# listing
+ASSETS_GRAPHQL_QUERY_TEMPLATE = """
 query {{
-    exposures(count:{page_count},page:{page_num}) {{
+    assets(s:[{{rel:"assetGroup", field:"name", dir:ASC}}],count:{page_count},page:{page_num}) {{
         edges {{
             node {{
-                __typename,
-                ... on BaseExposure {{
-                    id,
-                    name,
-                    exposureUrl,
-                    isIgnored,
-                    currentIncidentUuid,
-                    lastDetected,
-                    latestExposureEventType,
-                    assetGroup,
-                    category,
-                    created,
-                    definition,
-                    lastUpdated,
-                    probeId,
-                    severity,
-                    severityNormalized,
-                    signature {{
-                        ... on HTTPSignature {{
-                            categoryId,
-                            categoryName,
-                            created,
-                            cve,
-                            definitionName,
-                            description,
-                            enabled,
-                            followRedirects,
-                            hasTemplate,
-                            lastUpdated,
-                            name,
-                            recommendations,
-                            references,
-                            severity,
-                            signatureType,
-                            definition {{
-                                id,
-                                name,
-                                categoryId,
-                                categoryName,
-                                created,
-                                description,
-                                lastUpdated,
-                                signatureCount
+                ... on CloudAsset {{
+                    activeARecords {{
+                        edges {{
+                            node {{ 
+                                ... on ADnsRecord {{
+                                    id,
+                                    ipAddress
+                                }}
+                            }}
+                        }}
+                    }},
+                    activeCnameRecords {{
+                        edges {{
+                            node {{
+                                ... on CnameDnsRecord {{
+                                    id,
+                                    subdomain,
+                                    rawRecord
+                                }}
                             }}
                         }}
                     }}
-                    asset {{
-                        ... on SubdomainAsset {{
-                            id,
-                            host
+                }},
+                ... on IpAsset {{
+                    activeARecords {{
+                        edges {{
+                            node {{ 
+                                ... on ADnsRecord {{
+                                    id,
+                                    ipAddress
+                                }}
+                            }}
+                        }}
+                    }},
+                    activeCnameRecords {{
+                        edges {{
+                            node {{
+                                ... on CnameDnsRecord {{
+                                    id,
+                                    subdomain,
+                                    rawRecord
+                                }}
+                            }}
                         }}
                     }}
-                }}
+                }},
+                ... on SubdomainAsset {{
+                    activeARecords {{
+                        edges {{
+                            node {{ 
+                                ... on ADnsRecord {{
+                                    id,
+                                    ipAddress
+                                }}
+                            }}
+                        }}
+                    }},
+                    activeCnameRecords {{
+                        edges {{
+                            node {{
+                                ... on CnameDnsRecord {{
+                                    id,
+                                    subdomain,
+                                    rawRecord
+                                }}
+                            }}
+                        }}
+                    }}
+                }},
+                __typename,
+    	        ... on BaseAsset {{
+                    humanName,
+                    activeARecordCount,
+        	    activeCnameRecordCount,
+	            exposureRating,
+            	    hasUnmanagedExposures,
+	            activeARecordCount,
+            	    onlinePortEntryCount,
+                    isOnline,
+	            onlineDnsEntryCount,
+	            onlineTechnologyCount,
+                    canBeMonitored,
+	            assetGroupId,
+	            assetGroupName,
+	            assetType,
+	            created,
+	            geoData {{
+                        id,
+                        city,
+                        country
+                    }},
+	            host,
+                    ... on IpAsset {{
+                        ipAddress,
+                        technologies {{
+                            edges {{
+                                node {{
+                                    name
+                                }}
+                            }}
+                        }},
+                        services {{
+                            edges {{
+                                node {{
+                                    name,
+                                    port,
+                                    isActive,
+                                    lastActive
+                                }}
+                            }}
+                        }}
+                    }},
+                    ... on CloudAsset {{
+                        ipAddress,
+                        technologies {{
+                            edges {{
+                                node {{
+                                    name
+                                }}
+                            }}
+                        }},
+                        services {{
+                            edges {{
+                                node {{
+                                    name,
+                                    port,
+                                    isActive,
+                                    lastActive
+                                }}
+                            }}
+                        }}
+                    }},
+                    ... on SubdomainAsset {{
+                       ipAddress,
+                       technologies {{
+                            edges {{
+                                node {{
+                                    name
+                                }}
+                            }}
+                        }},
+                        services {{
+                            edges {{
+                                node {{
+                                    name,
+                                    port,
+                                    isActive,
+                                    lastActive
+                                }}
+                            }}
+                        }}
+                    }},
+	            id,
+	            importance,
+ 	            isMonitored,
+    	            lastUpdated,
+	            notificationsEnabled,
+	            parentName,
+	            risk,
+	            verifiedStatus,
+                    assetGroup {{
+                        id,
+                        name
+                    }}
+	        }}
             }}
         }},
         pageInfo {{
@@ -128,16 +240,24 @@ def validate_input(helper, definition):
     pass
 
 def collect_events(helper, ew):
+    
+    # Get the values for all the user supplied options
     opt_assetnote_api_key = helper.get_arg('assetnote_api_key')
     opt_assetnote_instance = helper.get_arg('assetnote_instance')
+    opt_sleep_time_per_page = int(helper.get_arg('sleep_time_per_page'))
+    opt_num_retries_per_page = int(helper.get_arg('num_retries_per_page'))
+    opt_backoff_time_per_page_retry = int(helper.get_arg('backoff_time_per_page_retry'))
     
+    # define a global parameters set
     all_params = {'assetnote_index': helper.get_output_index(),
                   'assetnote_sourcetype': helper.get_sourcetype(),
                   'assetnote_source': helper.get_input_type(),
                   'assetnote_api_key': opt_assetnote_api_key,
                   'assetnote_instance': opt_assetnote_instance,
-                  'sleep_time': SLEEP_TIMEOUT_PER_PAGE,
-                  'page_count': EXPOSURES_PER_PAGE_COUNT,
+                  'sleep_time': opt_sleep_time_per_page,
+                  'backoff_time': opt_backoff_time_per_page_retry,
+                  'num_retries': opt_num_retries_per_page,
+                  'page_count': ASSETS_PER_PAGE_COUNT,
                   'limit_pages_returned': LIMIT_PAGES_RETURNED}
     
     # Printing all the current parameters to internal log
@@ -148,36 +268,73 @@ def collect_events(helper, ew):
     info(helper, all_params, msg)
 
     info(helper, all_params,
-         "Requesting exposures for instance: {assetnote_instance} page-wise...")
-    exposures = []
+         "Requesting assets for instance: {assetnote_instance} page-wise...")
+    assets = []
     get_next_page = True
     all_params['page_num'] = 1
     
     while get_next_page:
-
-        info(helper, all_params, 
-             "Requesting page: {page_num} for exposures from AssetNote...")
-        graphql_query = EXPOSURES_GRAPHQL_QUERY_TEMPLATE.format(**all_params)
-        url_to_call = "https://{assetnote_instance}.assetnotecloud.com/api/v2/graphql".format(**all_params)
-        method = "POST"
-        headers={
-            "X-ASSETNOTE-API-KEY": "{assetnote_api_key}".format(**all_params)
-        }
-        payload = dict(query=graphql_query)
-        resp = helper.send_http_request(url=url_to_call,
-                                        method=method, 
-                                        payload=payload,
-                                        headers=headers,
-                                        verify=True)
         
-        info(helper, all_params, 
-             "Parsing response from page: {page_num} from AssetNote...")
-        status_code = resp.status_code
-        resp_json = resp.json()
-
+        all_params['try'] = 0
+        all_params['page_load_success'] = False
+        
+        while not all_params['page_load_success'] \
+            and all_params['try'] < all_params['num_retries']:
+            
+            all_params['try'] += 1
+            
+            info(helper, all_params, 
+                 "Try: {try}. Requesting page: {page_num} for assets from AssetNote...")
+            graphql_query = ASSETS_GRAPHQL_QUERY_TEMPLATE.format(**all_params)
+            url_to_call = "https://{assetnote_instance}.assetnotecloud.com/api/v2/graphql".format(**all_params)
+            method = "POST"
+            headers={
+                "X-ASSETNOTE-API-KEY": "{assetnote_api_key}".format(**all_params)
+            }
+            payload = dict(query=graphql_query)
+            try:
+                # Attempt to make HTTP request to load the page with assets
+                resp = helper.send_http_request(url=url_to_call,
+                                                method=method, 
+                                                payload=payload,
+                                                headers=headers,
+                                                verify=True,
+                                                use_proxy=True)
+                status_code = resp.status_code
+                resp_text = resp.text
+                
+                if status_code == 200:
+                    all_params['page_load_success'] = True
+                else:
+                    all_params['page_load_success'] = False
+                    info(helper, all_params, 
+                        "Sleeping {backoff_time}s before requesting same page...")
+                    time.sleep(all_params['backoff_time'])
+                    
+            except Exception as e:
+                
+                # Log the exception occurred to log file
+                status_code = -1
+                err_class = str(e.__class__)
+                raw_err_msg = str(e)
+                err_msg  = "Error in send_http_request for page: {page_num}. "
+                err_msg += "Error: {}, {}".format(err_class, raw_err_msg)
+                info(helper, all_params, err_msg)
+                
+                # Page was not loaded successfully, so wait for some time before
+                # re-requesting the page
+                all_params['page_load_success'] = False
+                info(helper, all_params, 
+                        "Sleeping {backoff_time}s before requesting same page...")
+                time.sleep(all_params['backoff_time'])
+            
         info(helper, all_params,
             "Checking if page: {page_num} obtained successfully...")
-        if status_code != 200 or "data" not in resp_json:
+            
+        if status_code == -1:
+            get_next_page = True
+            
+        elif status_code != 200 or "data" not in resp_text:
             
             info(helper, all_params, "Error encountered when retrieving page: {page_num}...")
             info(helper, all_params, "Error: ")
@@ -188,31 +345,30 @@ def collect_events(helper, ew):
         else:
 
             info(helper, all_params,
-                 "Parsing page: {page_num} response for exposures...")
-                
-            #info(helper, all_params, str(resp_json),
-            #     format_params=False)
+                 "Parsing page: {page_num} response for assets as JSON...")
+                 
+            resp_json = resp.json()    
             
             info(helper, all_params, 
-                "Listing the number of exposures on the page obtained...")
-            exposures_on_page = resp_json['data']['exposures']['edges']
-            for exposure_on_page in exposures_on_page:
-                exposures.append(exposure_on_page)
-                all_params['exposures_count'] = len(exposures)
+                "Listing the number of assets on the page obtained...")
+            assets_on_page = resp_json['data']['assets']['edges']
+            for asset_on_page in assets_on_page:
+                assets.append(asset_on_page)
+                all_params['assets_count'] = len(assets)
                 
             info(helper, all_params, 
-                "Number of exposures after page: {page_num} is: {exposures_count}")
+                "Number of assets after page: {page_num} is: {assets_count}")
 
             info(helper, all_params,
                 "Checking if another page exists from page: {page_num} response...")
                 
             info(helper, all_params, 
-                 "Calculating the number of exposures in the page...")
-            all_params['exposure_count_per_page'] = len(exposures_on_page)
+                 "Calculating the number of assets in the page...")
+            all_params['asset_count_per_page'] = len(assets_on_page)
             
             info(helper, all_params, 
-                 "Creating all {exposure_count_per_page} exposures as an event...")
-            new_event = helper.new_event(json.dumps(exposures_on_page, indent=4),
+                 "Creating all {asset_count_per_page} assets as an event...")
+            new_event = helper.new_event(json.dumps(assets_on_page, indent=4),
                             index=all_params['assetnote_index'],
                             sourcetype=all_params['assetnote_sourcetype'],
                             source=all_params['assetnote_source'])
@@ -224,7 +380,7 @@ def collect_events(helper, ew):
             
             info(helper, all_params, 
                     "Checking if next page should be obtained...")
-            get_next_page_in_resp = resp_json['data']['exposures']['pageInfo']['hasNextPage']
+            get_next_page_in_resp = resp_json['data']['assets']['pageInfo']['hasNextPage']
             if get_next_page_in_resp:
                 
                 info(helper, all_params, 
@@ -246,6 +402,9 @@ def collect_events(helper, ew):
                 info(helper, all_params, 
                      "Incrementing page counter...") 
                 all_params['page_num'] += 1
+                
+                info(helper, all_params, 
+                     "Next page to get: {page_num}...")
                 
                 info(helper, all_params, 
                      "Sleeping for {sleep_time}s before requesting next page...")
